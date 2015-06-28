f2d9bdbb2259afd7103a674a180db6bad1348115
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-28 14:56:06 +0200

    Add Xcode project

---
+++ DiskCache/Source/DiskCache.h
@@ -0,0 +1,15 @@
+//
+//  DiskCache.h
+//  DiskCache
+//
+//  Created by Julian Grosshauser on 28/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiskCache.
+FOUNDATION_EXPORT double DiskCacheVersionNumber;
+
+//! Project version string for DiskCache.
+FOUNDATION_EXPORT const unsigned char DiskCacheVersionString[];
---
+++ DiskCacheTests/Source/Classes/DiskCacheTests.swift
@@ -0,0 +1,20 @@
+//
+//  DiskCacheTests.swift
+//  DiskCacheTests
+//
+//  Created by Julian Grosshauser on 28/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+
+class DiskCacheTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
