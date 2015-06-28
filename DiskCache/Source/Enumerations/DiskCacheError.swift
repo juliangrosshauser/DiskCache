03995d9bfed335e50e2454a203a62bc4f9b58778
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-28 17:43:02 +0200

    Add `DiskCacheError` enum

---
+++ DiskCache/Source/Enumerations/DiskCacheError.swift
@@ -0,0 +1,16 @@
+//
+//  DiskCacheError.swift
+//  DiskCache
+//
+//  Created by Julian Grosshauser on 28/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+/**
+Contains all `ErrorType`s `DiskCache` can throw
+*/
+public enum DiskCacheError: ErrorType {
+
+    case EmptyKey
+    case ProblemCreatingFile
+}
