812a9c646fb32c9c5ea22c1293e622fc8c01b7c4
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-28 17:44:47 +0200

    Add `cacheData(_:forKey:completionHandler:)`

--- DiskCache/Source/Classes/DiskCache.swift
+++ DiskCache/Source/Classes/DiskCache.swift
@@ -40,4 +40,45 @@ public class DiskCache {
     public convenience init() {
         self.init(identifier: "DiskCache")
     }
+
+    //MARK: Cache data
+
+    /**
+    Cache data asynchronously
+
+    - Parameter data: Data to cache
+    - Parameter forKey: Key for data
+    - Parameter completionHandler: Called on main thread after data is cached
+    
+    - Warning: Doesn't throw when error happens asynchronously. Check `.Success` or `.Failure` in `Result` parameter of `completionHandler` instead.
+    */
+    public func cacheData(data: NSData, forKey key: String, completionHandler: (Result -> Void)?) throws {
+        if key.isEmpty {
+            throw DiskCacheError.EmptyKey
+        }
+
+        dispatch_async(ioQueue) {
+            if !self.fileManager.fileExistsAtPath(self.path) {
+                do {
+                    try self.fileManager.createDirectoryAtPath(self.path, withIntermediateDirectories: true, attributes: nil)
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completionHandler?(.Failure(error))
+                    }
+                }
+            }
+
+            let filePath = self.path.stringByAppendingPathComponent(key)
+
+            if self.fileManager.createFileAtPath(filePath, contents: data, attributes: nil) {
+                dispatch_async(dispatch_get_main_queue()) {
+                    completionHandler?(.Success)
+                }
+            } else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    completionHandler?(.Failure(DiskCacheError.ProblemCreatingFile))
+                }
+            }
+        }
+    }
 }
