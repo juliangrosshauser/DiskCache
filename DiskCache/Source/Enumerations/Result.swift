b37edf8a0c7fbcf9a3a3c0325f3c097640a66ae4
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-28 17:44:25 +0200

    Add `Result` enum

---
+++ DiskCache/Source/Enumerations/Result.swift
@@ -0,0 +1,16 @@
+//
+//  Result.swift
+//  DiskCache
+//
+//  Created by Julian Grosshauser on 28/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+/**
+Encapsulates success or failure
+*/
+public enum Result {
+
+    case Success
+    case Failure(ErrorType)
+}
